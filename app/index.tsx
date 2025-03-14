26310430d9a53c33f2c9508563d486cdc83cd940
Aritra1235 <aritra1235@gmail.com>
2025-03-14 23:39:31 +0530

    formating for better readability

--- app/index.tsx
+++ app/index.tsx
@@ -1,64 +1,70 @@
-import { TextInput, StyleSheet, KeyboardAvoidingView, Platform, Button, TouchableOpacity } from "react-native";
+import {
+  TextInput,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableOpacity,
+} from "react-native";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
-import {fetch} from 'expo/fetch'
+import { fetch } from "expo/fetch";
 import { useState } from "react";
 import { router, Router } from "expo-router";
 import { storeData } from "@/utils/storage";
 
-
-
 export default function Index() {
-  const [errorMessage, setErrorMessage] = useState('');
-  const [serverIp, setServerIp] = useState('');
+  const [errorMessage, setErrorMessage] = useState("");
+  const [serverIp, setServerIp] = useState("");
   const [isError, setIsError] = useState(false);
 
-
   async function checkServer() {
-    console.log('Checking server');
-    if (/^(https?:\/\/(?:\d{1,3}(?:\.\d{1,3}){3}|\S+):\d+)(\/?)$/.test(serverIp)) {
-      console.log('Valid Url', serverIp);
+    console.log("Checking server");
+    if (
+      /^(https?:\/\/(?:\d{1,3}(?:\.\d{1,3}){3}|\S+):\d+)(\/?)$/.test(serverIp)
+    ) {
+      console.log("Valid Url", serverIp);
     } else {
-      console.log('Invalid Url', serverIp)
-      setErrorMessage('Invalid Url')
-      setIsError(true)
+      console.log("Invalid Url", serverIp);
+      setErrorMessage("Invalid Url");
+      setIsError(true);
       return 0;
     }
 
-    console.log('Checking server', serverIp);
+    console.log("Checking server", serverIp);
     await fetch(`${serverIp}/System/Info/Public`, {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
+        "Content-Type": "application/json",
+        Accept: "application/json",
       },
       //body: JSON.stringify({}),
-    }).then((response) => {
-      console.log('Response', response);
-      if (response.status === 200) {
-        return response.json(); // here i will parse the json
-      } else {
-        console.log('Server not found');
-        throw new Error('Server not found');
-      }
     })
-    .then((data) => {
-      console.log('Server info:', data);
-      console.log('Server name:', data.ServerName);
-      console.log('Version:', data.Version);
-      
-      setErrorMessage(`Connected to ${data.ServerName} (${data.Version})`);
-      setIsError(false);
-      storeData('serverIp', serverIp);
-      storeData('serverName', data.ServerName);
-      storeData('serverVersion', data.Version);
-      router.push('/login');
-    })
-    .catch((error) => {
-      console.error(error);
-      setErrorMessage(error.message);
-      setIsError(true);
-    });
+      .then((response) => {
+        console.log("Response", response);
+        if (response.status === 200) {
+          return response.json(); // here i will parse the json
+        } else {
+          console.log("Server not found");
+          throw new Error("Server not found");
+        }
+      })
+      .then((data) => {
+        console.log("Server info:", data);
+        console.log("Server name:", data.ServerName);
+        console.log("Version:", data.Version);
+
+        setErrorMessage(`Connected to ${data.ServerName} (${data.Version})`);
+        setIsError(false);
+        storeData("serverIp", serverIp);
+        storeData("serverName", data.ServerName);
+        storeData("serverVersion", data.Version);
+        router.push("/login");
+      })
+      .catch((error) => {
+        console.error(error);
+        setErrorMessage(error.message);
+        setIsError(true);
+      });
   }
 
   return (
@@ -66,7 +72,9 @@ export default function Index() {
       behavior={Platform.OS === "ios" ? "padding" : "height"}
       style={{ flex: 1 }}
     >
-      <ThemedView style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <ThemedView
+        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+      >
         <ThemedText style={styles.welcome}>Welcome</ThemedText>
         <ThemedText style={styles.instructions}>
           This Is A Simple Mobile Music Client For JellyFin
@@ -81,14 +89,14 @@ export default function Index() {
           gap: 5,
         }}
       >
-        <ThemedText style={styles.instructions}>Enter Server Ip/domain Including protocol</ThemedText>
-        {errorMessage ? <ThemedText style={styles.instructions}>{errorMessage}</ThemedText> : null}
+        <ThemedText style={styles.instructions}>
+          Enter Server Ip/domain Including protocol
+        </ThemedText>
+        {errorMessage ? (
+          <ThemedText style={styles.instructions}>{errorMessage}</ThemedText>
+        ) : null}
         <TextInput
-          style={[
-            styles.input,
-            {borderColor: isError ? 'red' : '#444648'}
-          ]}
-          
+          style={[styles.input, { borderColor: isError ? "red" : "#444648" }]}
           placeholder="Enter Server Address"
           placeholderTextColor="#A0A0A0"
           value={serverIp}
@@ -100,15 +108,14 @@ export default function Index() {
         {/*<Button title="Submit" onPress={() => {
           checkServer();
         }} />*/}
-        <TouchableOpacity 
+        <TouchableOpacity
           style={styles.submit}
           onPress={() => {
             checkServer();
-        }}>
+          }}
+        >
           <ThemedText style={styles.buttonText}>Submit</ThemedText>
         </TouchableOpacity>
-
-
       </ThemedView>
     </KeyboardAvoidingView>
   );
@@ -133,20 +140,19 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15,
     fontSize: 16,
     marginBottom: 10,
-    
   },
   submit: {
-    backgroundColor: 'transparent',
+    backgroundColor: "transparent",
     padding: 10,
     borderRadius: 5,
-    borderColor: 'transparent',
+    borderColor: "transparent",
     borderWidth: 1,
     width: 280,
-    alignItems: 'center',
+    alignItems: "center",
   },
   buttonText: {
     color: "#007AFF",
     fontSize: 16,
     fontWeight: "600",
-  }
+  },
 });
